7d556973f9079de0c5635880dee9dcc464edfba0
popstarfreas <6698249+popstarfreas@users.noreply.github.com>
2023-09-10 12:11:37 +0100

    chore: Update

--- build.plugin.js
+++ build.plugin.js
@@ -1,6 +1,7 @@
 var fs = require("fs");
 var child_process = require("child_process");
 
-child_process.execSync("npm i ../../pluginreference")
-child_process.execSync("npm run build")
+child_process.execSync("yarn remove terrariaserver-lite", { stdio: "inherit" })
+child_process.execSync("yarn add ../../pluginreference", { stdio: "inherit" })
+child_process.execSync("npm run build", { stdio: "inherit" })
 fs.renameSync("./build", "./plugin")
