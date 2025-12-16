0820b745062a7705af6ed12d01cf71d6a71f7c1f
popstarfreas <popstarfreas2@gmail.com>
2025-12-16 09:43:38 +0000

    feat: Update to latest TSL

--- build.plugin.js
+++ build.plugin.js
@@ -1,7 +1,8 @@
-var fs = require("fs");
-var child_process = require("child_process");
+import fs from "node:fs";
+import child_process from "node:child_process";
 
-child_process.execSync("yarn remove terrariaserver-lite", { stdio: "inherit" })
+child_process.execSync("pnpm i", { stdio: "inherit" })
+child_process.execSync("pnpm remove terrariaserver-lite", { stdio: "inherit" })
 child_process.execSync("yarn add ../../pluginreference", { stdio: "inherit" })
-child_process.execSync("yarn build:packed", { stdio: "inherit" })
+child_process.execSync("pnpm build:packed", { stdio: "inherit" })
 fs.renameSync("./output", "./plugin")
