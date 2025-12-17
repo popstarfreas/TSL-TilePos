34caac2e33837f6c7bdf7d6cbac14f5bcda327b0
popstarfreas <popstarfreas2@gmail.com>
2025-12-17 11:10:58 +0000

    build: Fix using yarn instead of pnpm

--- build.plugin.js
+++ build.plugin.js
@@ -3,6 +3,6 @@ import child_process from "node:child_process";
 
 child_process.execSync("pnpm i", { stdio: "inherit" })
 child_process.execSync("pnpm remove terrariaserver-lite", { stdio: "inherit" })
-child_process.execSync("yarn add ../../pluginreference", { stdio: "inherit" })
+child_process.execSync("pnpm add ../../pluginreference", { stdio: "inherit" })
 child_process.execSync("pnpm build:packed", { stdio: "inherit" })
 fs.renameSync("./output", "./plugin")
